06c595846aa41c86b3a1c12eb1aa872e48d59c82
S ravi G <sravig@Ss-MacBook-Pro.local>
2020-12-13 22:02:16 -0600

    animal names updated

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ const App = () => {
   return React.createElement("div", { id: "something-important!" }, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: "tiger1",
-      animal: "Dog1",
-      breed: "tails1",
+      name: "twinkle",
+      animal: "tiger",
+      breed: "indian",
     }),
     React.createElement(Pet, {
-      name: "tiger2",
-      animal: "Dog2",
-      breed: "tails2",
+      name: "maddy",
+      animal: "cat",
+      breed: "african",
     }),
     React.createElement(Pet, {
-      name: "tiger3",
-      animal: "Dog3",
-      breed: "tails3",
+      name: "slobby",
+      animal: "crocodile",
+      breed: "reptile",
     }),
   ]);
 };
