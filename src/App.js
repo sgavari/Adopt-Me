aa450ed94000e59c137ba3ee5e5e8b15a6355ea1
S ravi G <sravig@Ss-MacBook-Pro.local>
2020-12-17 15:43:07 -0600

    link and router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import SearchParams from './SearchParams'
+import { Link, Router } from "@reach/router"
+import Details from './Details';
 
 const App = () => {
 return(
   <React.StrictMode>
 <div>
-  <h1 id= "something-important!">Adopt Me!</h1>
-  <SearchParams></SearchParams>
+<header>
+  <Link to="/">Adopt Me!</Link>
+  </header> 
+  <Router>
+  <SearchParams path="/"/>
+  <Details path = "/details/:id"/>
+  </Router>
 </div>
 </React.StrictMode>
 );
